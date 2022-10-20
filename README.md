cd439c2acf42e07e721b9fece550bfd494833a10
Snezhe4ka <dovha2802@gmail.com>
2022-10-20 14:15:15 +0200

    Added login and registration form

---
+++ README.md
@@ -0,0 +1,16 @@
+#  IT site
+
+IT site  - place where you can post info about different languages in development. Was  built by using Django framework with database SQLiteStudio
+
+
+
+Run app
+====================
+1. Clone repository
+2. Setup virtual environment
+3. Run the following commands
+```
+  pip install requirements.txt
+  python manage.py migrate
+  python manage.py runserver
+```
---
+++ mysite/news/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
+    path('contact/', contact, name='contact'),
+    # path('', index, name='home'),
+    path('', HomeNews.as_view(), name='home'),
+    # path('category/<int:category_id>/', get_category, name='category'),
+    path('category/<int:category_id>/', NewsByCategory.as_view(), name='category'),
+    path('news/<int:news_id>/', view_news, name='view_news'),
+    path('news/add-news/', add_news, name='add_news'),
+
+
+
+]
